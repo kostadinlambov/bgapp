c70511b2ba4f60f8eabf7056d6997adadba94ebd
kostadinlambov <klambov20@gmail.com>
2022-03-13 21:30:58 +0100

    Added Jenkins pipeline

---
+++ M5-Jenkins-Advanced/install_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "* Add required packages"
+apt-get update
+apt-get install -y ca-certificates curl gnupg lsb-release git
+
+echo "* Add repository key"
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "* Add the Docker repository"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "* Install the packages (Java, git, Docker)"
+apt-get update
+apt-get install -y fontconfig openjdk-11-jre git docker-ce docker-ce-cli containerd.io
+
+echo "* Adjust the group membership"
+usermod -aG docker vagrant
+
+# echo "* Install Docker Compose"
+# mkdir -p /home/vagrant/.docker/cli-plugins/
+# curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o /home/vagrant/.docker/cli-plugins/docker-compose
+# chmod +x /home/vagrant/.docker/cli-plugins/docker-compose
+
+echo "* Install Docker Compose V2 for all users"
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
+echo "* Get docker-compose version"
+docker compose version
---
+++ M5-Jenkins-Advanced/install_jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "* Update repositories and install Java"
+apt-get update
+apt-get install -y fontconfig openjdk-11-jre git
+
+echo "* Add and import the repository key"
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+echo "* Update repositories and install Jenkins"
+apt-get update
+apt-get install -y jenkins
+
+echo "* admin password is:"
+cat /var/lib/jenkins/secrets/initialAdminPassword
